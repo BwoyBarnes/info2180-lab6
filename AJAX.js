002cea077764cbc11f6d7f598b3572a544bdcfec
BwoyBarnes <odanebarnes@outlook.com>
2016-11-09 09:49:42 -0500

    Exercise 3 - Look up any word

--- AJAX.js
+++ AJAX.js
@@ -1,14 +1,15 @@
 "use strict";
 function loadDefinitions()
 {
+    var query = document.getElementById("queryTxtBox").value.toLowerCase();
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() 
     {
         if (this.readyState == 4 && this.status == 200) 
         {
-            alert(this.responseText);
+            document.getElementById("result").innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "request.php?q="+"definition", true);
+    xhttp.open("GET", "request.php?q="+ query, true);
     xhttp.send();
 }
